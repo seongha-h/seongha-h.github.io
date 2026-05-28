0d298eabcca5d6c161bbd03192de81e7393459c1
seongha-h <seongha31@snu.ac.kr>
2026-05-28 06:03:43 +0000

    hw12-1

---
+++ assets/js/hw12.js
@@ -0,0 +1,35 @@
+// [숙제12] 인문학 데이터 시각화
+// 2024-14607 김성하
+
+// Q1
+fetch("/data/sillok.json")
+.then(response => response.json())
+.then(records => {
+    const 
+
+});
+function drawChart(rows){
+    const labels = rows.map(r=>r.king);
+    const counts = rows.map(r=>r.volumes);
+    const canvas document.querySelector("#q1-chart")
+    new CharacterData(canvas,{
+        type:"bar",
+        data:{
+            labels:labels,
+            datasets:[{
+                label:"조선왕조실록 왕대별 권수",
+                data:counts}],
+                backgroundColor:["rgba(54, 162, 235, 0.6)"]
+        options:{
+            plugins:{
+                title:{display:true, text:"조선왕조실록 왕대별 권수"}
+            },
+            scales:{
+                y:{beginsAtZero:true}
+            }
+        }
+        }
+    })
+}
+
+/
