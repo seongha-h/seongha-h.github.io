cbfd139b03366479f2ae4f7c7e0585d16cc00dda
seongha-h <seongha31@snu.ac.kr>
2026-05-28 06:51:55 +0000

    1번 수정

--- assets/js/hw12.js
+++ assets/js/hw12.js
@@ -3,31 +3,39 @@
 
 // Q1
 fetch("/data/sillok.json")
-.then(response => response.json())
-.then(records => {drawChart(records)
-});
+  .then(response => response.json())
+  .then(records => {
+    drawChart(records);
+  });
 
-function drawChart(rows){
-    const labels = rows.map(r=>r.king);
-    const counts = rows.map(r=>r.volumes);
-    const canvas document.querySelector("#q1-chart")
-    new CharacterData(canvas,{
-        type:"bar",
-        data:{
-            labels:labels,
-            datasets:[{
-                label:"조선왕조실록 왕대별 권수",
-                data:counts}],
-                backgroundColor:["rgba(54, 162, 235, 0.6)"]
-        options:{
-            plugins:{
-                title:{display:true, text:"조선왕조실록 왕대별 권수"}
-            },
-            scales:{
-                y:{beginsAtZero:true}
-            }
+function drawChart(rows) {
+  const labels = rows.map(r => r.king);
+  const counts = rows.map(r => r.volumes);
+
+  const canvas = document.querySelector("#q1-chart");
+
+  new Chart(canvas, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "조선왕조실록 왕대별 권수",
+        data: counts,
+        backgroundColor: "rgba(54, 162, 235, 0.6)"
+      }]
+    },
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: "조선왕조실록 왕대별 권수"
         }
+      },
+      scales: {
+        y: {
+          beginAtZero: true
         }
-    })
+      }
+    }
+  });
 }
-
