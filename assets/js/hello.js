84dd20b5ee34377778745dcb95ef6c4fc5fff3ac
seongha-h <seongha31@snu.ac.kr>
2026-07-11 04:28:20 +0000

    필요없는 사이트 지우기 2

--- assets/js/hello.js
+++
@@ -1,8 +0,0 @@
-const owner = "김성하";
-const field = "언어데이터";
-const year = 2026;
-
-
-console.log(`안녕하세요! ${owner}의 블로그에 오신 것을 환영합니다!`);
-console.log(`해당 웹사이트는 ${field}와 관련된 내용이 포스팅 될 예정입니다`);
-console.log(`해당 웹사이트는 ${year}에 개설되었습니다`);
